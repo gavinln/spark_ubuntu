86e4f3f50f0e96f601154078948960f13422db62
Gavin Noronha <gnoronha@financialengines.com>
2014-12-26 18:05:00 -0800

    Added function to process ngrams in spark

---
+++ docker/spark/spark_ngrams.py
@@ -0,0 +1,39 @@
+
+
+def process_ngram(record):
+    expected_tokens = record[0]
+    data = record[1].split('\t')
+
+    # error checking
+    if len(data) < 3:
+        return []
+
+    # unpack data
+    ngram = data[0].split()
+    year = data[1]
+    count = int(data[2])
+
+    # more error checking
+    if len(ngram) != expected_tokens:
+        return []
+
+    # generate key
+    pair = tuple(sorted([ngram[0], ngram[self.expected_tokens - 1]]))
+    k = pair + (year,)
+
+    return [(k, count)]
+
+
+def process_ngram_cheat(record):
+    try:
+        data = record.split('\t')
+        # unpack data
+        ngram = data[0].split()
+        year = data[1]
+        count = int(data[2])
+        # generate key
+        pair = tuple(sorted([ngram[0], ngram[-1]]))
+        k = pair + (year,)
+        return [('\t'.join(k), count)]
+    except Exception:
+        return []
