a7b50babbbb3e2197a9c9524c59ff600e715cfdc
Gavin Noronha <gavinln@hotmail.com>
2014-12-28 13:05:45 -0800

    Renamed files to more appropriate names

--- python/server_list.py
+++ python/server_list.py
@@ -2,7 +2,9 @@ from flask import Flask
 from flask import render_template
 import flask
 
+from yarn_api_client.resource_manager import ResourceManager
 import urllib
+from collections import namedtuple
 
 
 app = Flask(__name__)
@@ -54,20 +56,45 @@ class ServerList(list):
         return '\n'.join(str(server) for server in self)
 
 
+def getClusterInfo():
+    rm = ResourceManager(address='localhost', port=8088)
+    data = rm.cluster_information().data
+    return data.get('clusterInfo', None)
+
+YarnApp = namedtuple('YarnAppInfo', ['id', 'trackingUrl', 'state'])
+
+
+def getYarnApps():
+    yarnApps = []
+    rm = ResourceManager(address='localhost', port=8088)
+    data = rm.cluster_applications().data
+    apps = data.get('apps', None)
+    if apps:
+        appList = apps.get('app', None)
+        if appList:
+            for app in appList:
+                yarnApps.append(YarnApp._make((
+                    app['id'],
+                    app['trackingUrl'],
+                    app['state'])))
+    return yarnApps
+
+
 @app.route('/')
-def hello_world():
+def server_list():
     urlHostPort = flask.request.host.split(':')
     url_base = 'http://%s' % urlHostPort[0]
 
     hadoopList = ServerList.initialize(hadoop)
     yarnList = ServerList.initialize(yarn)
-    return render_template('hello1.html',
+    yarnApps = getYarnApps()
+    return render_template('server_list.html',
                            url_base=url_base,
                            hadoop=hadoopList,
-                           yarn=yarnList)
+                           yarn=yarnList,
+                           yarnApps=yarnApps)
 
 
 if __name__ == '__main__':
     # port: 5000
     app.run(host='0.0.0.0')
-    #hello_world()
