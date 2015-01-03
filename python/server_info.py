6c326ea85145a64b4a2432ba85ac7cfefcc1ecdd
Gavin Noronha <gavinln@hotmail.com>
2015-01-03 14:18:20 -0800

    refactored flask code

---
+++ python/server_info.py
@@ -0,0 +1,109 @@
+import urllib
+from collections import namedtuple
+
+from yarn_api_client.resource_manager import ResourceManager
+
+hadoop = (
+    ('NameNode', 50070),
+    ('DataNode', 50075),
+    ('Secondary NameNode', 50090),
+)
+
+yarn = (
+    ('Resource manager', 8088),
+)
+
+
+class ServerInfo(object):
+    @classmethod
+    def initialize(cls, serverData):
+        return cls(serverData[0], serverData[1], None)
+
+    def __init__(self, server, port, status):
+        self.name = server
+        self.port = port
+        try:
+            code = urllib.urlopen("http://127.0.0.1:%s" % self.port).getcode()
+            if code == 200:
+                self.status = True
+            else:
+                self.status = False
+        except:
+            self.status = False
+
+    def __str__(self):
+        names = ['name', 'port', 'status']
+        values = [self.name, self.port, self.status]
+        return ', '.join('%s: %s' % (
+            name, value) for name, value in zip(names, values))
+
+
+class ServerList(list):
+    @classmethod
+    def initialize(cls, serverData):
+        servers = cls()
+        for server in serverData:
+            servers.append(ServerInfo.initialize(server))
+        return servers
+
+    def __str__(self):
+        return '\n'.join(str(server) for server in self)
+
+
+YarnApp = namedtuple('YarnAppInfo', ['id', 'name', 'port_path', 'state'])
+
+
+def getYarnApplicationsData(resourceManager):
+    try:
+        apps = resourceManager.cluster_applications()
+    except Exception:
+        return None
+    return apps.data
+
+
+def getYarnApps():
+    yarnApps = []
+    rm = ResourceManager(address='localhost', port=8088)
+    data = getYarnApplicationsData(rm)
+    if data:
+        try:
+            apps = data['apps']
+            if apps:
+                appList = apps['app']
+                for app in appList:
+                    url = app['trackingUrl']
+                    port_path = url.split(':')[2]
+                    #hostPort = url.split(':')
+                    #url2 = ':'.join([url_base, hostPort[2]])
+                    yarnApps.append(YarnApp._make((
+                        app['id'],
+                        app['name'],
+                        port_path,
+                        app['state'])))
+        except KeyError:
+            pass
+
+    return yarnApps
+
+
+def getHadoopServers():
+    return ServerList.initialize(hadoop)
+
+
+def getYarnServers():
+    return ServerList.initialize(yarn)
+
+
+def test_hadoopServers():
+    print(getHadoopServers())
+    assert True
+
+
+def test_yarnServers():
+    print(getYarnServers())
+    assert True
+
+
+def test_yarnApps():
+    print(getYarnApps())
+    assert False
--- python/server_list.py
+++ python/server_list.py
@@ -1,106 +1,33 @@
+from __future__ import print_function
+
+import flask
 from flask import Flask
 from flask import render_template
-import flask
 
-from yarn_api_client.resource_manager import ResourceManager
-import urllib
-from collections import namedtuple
+from server_info import getYarnApps
+from server_info import getHadoopServers
+from server_info import getYarnServers
 
 
 app = Flask(__name__)
 
-hadoop = (
-    ('NameNode', 50070),
-    ('DataNode', 50075),
-    ('Secondary NameNode', 50090),
-)
-
-yarn = (
-    ('Resource manager', 8088),
-)
-
-
-class ServerInfo(object):
-    @classmethod
-    def initialize(cls, serverData):
-        return cls(serverData[0], serverData[1], None)
-
-    def __init__(self, server, port, status):
-        self.name = server
-        self.port = port
-        try:
-            code = urllib.urlopen("http://127.0.0.1:%s" % self.port).getcode()
-            if code == 200:
-                self.status = True
-            else:
-                self.status = False
-        except:
-            self.status = False
-
-    def __str__(self):
-        names = ['name', 'port', 'status']
-        values = [self.name, self.port, self.status]
-        return ', '.join('%s: %s' % (
-            name, value) for name, value in zip(names, values))
-
-
-class ServerList(list):
-    @classmethod
-    def initialize(cls, serverData):
-        servers = cls()
-        for server in serverData:
-            servers.append(ServerInfo.initialize(server))
-        return servers
-
-    def __str__(self):
-        return '\n'.join(str(server) for server in self)
-
-
-def getClusterInfo():
-    rm = ResourceManager(address='localhost', port=8088)
-    data = rm.cluster_information().data
-    return data.get('clusterInfo', None)
-
-YarnApp = namedtuple('YarnAppInfo', ['id', 'name', 'trackingUrl', 'state'])
-
-
-def getYarnApps(hostUrl):
-    urlHostPort = flask.request.host.split(':')
-    url_base = 'http://%s' % urlHostPort[0]
-
-    yarnApps = []
-    rm = ResourceManager(address='localhost', port=8088)
-    data = rm.cluster_applications().data
-    apps = data.get('apps', None)
-    if apps:
-        appList = apps.get('app', None)
-        if appList:
-            for app in appList:
-                hostPort = app['trackingUrl'].split(':')
-                url = ':'.join([url_base, hostPort[2]])
-                yarnApps.append(YarnApp._make((
-                    app['id'],
-                    app['name'],
-                    url,
-                    app['state'])))
-    return yarnApps
-
 
 @app.route('/')
 def server_list():
-    urlHostPort = flask.request.host.split(':')
+    hostUrl = flask.request.host
+    #hostUrl = '192.168.0.132:5000'
+
+    urlHostPort = hostUrl.split(':')
     url_base = 'http://%s' % urlHostPort[0]
 
-    hadoopList = ServerList.initialize(hadoop)
-    yarnList = ServerList.initialize(yarn)
-    yarnApps = getYarnApps(flask.request.host)
     return render_template('server_list.html',
                            url_base=url_base,
-                           hadoop=hadoopList,
-                           yarn=yarnList,
-                           yarnApps=yarnApps)
+                           hadoop=getHadoopServers(),
+                           yarn=getYarnServers(),
+                           yarnApps=getYarnApps())
 
 
 if __name__ == '__main__':
     # port: 5000
     app.run(host='0.0.0.0')
+    #server_list()
