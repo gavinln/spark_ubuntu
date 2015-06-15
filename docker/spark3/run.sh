f06a1c8b08b8ff3bd99553abc56c16615af058a6
Gavin Noronha <gnoronha@financialengines.com>
2015-06-15 15:51:46 -0700

    added new spark docker container

---
+++ docker/spark3/run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# sandbox is the host machine dns name
+#sudo docker run -it -p 8088:8088 -p 8042:8042 -h sandbox spark3 bash
+sudo docker run -it -p 8088:8088 -p 8042:8042 spark3 bash
